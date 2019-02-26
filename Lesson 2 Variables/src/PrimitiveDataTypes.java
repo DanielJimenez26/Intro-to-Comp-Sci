b8400429afca0403f48d0c6901803b5361e09f4b
djimenez <djimenez@HS-IL-10.MOHS.local>
2019-02-26 08:18:09 -0500

    Recent from 2/26/19 8am

--- Lesson 2 Variables/src/PrimitiveDataTypes.java
+++ Lesson 2 Variables/src/PrimitiveDataTypes.java
@@ -3,16 +3,33 @@ public class PrimitiveDataTypes {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    String Student = "Daniel Jimenez - MOHS Student";
-    String Bear = "Bear attacks";
-    int Bearr = -50;
-    String BeaR = "Best Bear trick";
-    String BeaRR = "Bears attacking me";
-    String BeaRRR = "Favorite Bear trained - Ted";
-    
-    System.out.println(Student);
-    System.out.println(Bear+" - "+Bearr+"/year");
-    System.out.println(BeaR+" - "+BeaRR);
-    System.out.println(BeaRRR);
+		//String name;
+		//name = "Daniel Jimenez";
+		//System.out.println(name);
+		//String town = "Mount Olive";
+		//System.out.println("Johnny Fast A-Lot");
+	    String ageS = "Age";
+		int ageI = 17;
+		String heightS = "Height";
+		String heightR = "Feet";
+		int heightI = 79;
+		String weightS = "Weight";
+		String weightR = "lbs";
+		int weightI = 600;
+		String gpA = "GPA";
+		double gpaS = 7.6;
+		
+		
+		System.out.println(ageS+" = "+ageI);
+		System.out.println(heightS+" = "+heightI+" "+heightR);
+		System.out.println(weightS+" = "+weightI+ " "+weightR);
+		System.out.println(gpA+" = "+gpaS);
+		 
+		
+		
+		
+		
+		
 	}
+
 }
---
+++ Lesson 2 Variables/src/PrimitiveDataTypes2.java
@@ -0,0 +1,18 @@
+
+public class PrimitiveDataTypes2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    String Student = "Daniel Jimenez - MOHS Student";
+    String Bear = "Bear attacks";
+    int Bearr = -50;
+    String BeaR = "Best Bear trick";
+    String BeaRR = "Bears attacking me";
+    String BeaRRR = "Favorite Bear trained - Ted";
+    
+    System.out.println(Student);
+    System.out.println(Bear+" - "+Bearr+"/year");
+    System.out.println(BeaR+" - "+BeaRR);
+    System.out.println(BeaRRR);
+	}
+}
