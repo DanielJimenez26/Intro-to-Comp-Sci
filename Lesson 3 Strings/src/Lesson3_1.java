6c5eecc93b68cdbc5639425e7d29c48ca7b81165
djimenez <djimenez@HS-D210-16.MOHS.local>
2019-02-26 12:14:42 -0500

    2/26/19

---
+++ Lesson 3 Strings/src/Lesson3_1.java
@@ -0,0 +1,35 @@
+
+public class Lesson3_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       String s = "The rabbits is ";
+       int argh = 129;
+       String punc = ".";
+       String report = s + argh + punc;
+       System.out.println(report);
+       
+       String p = "GroovyDude";
+       System.out.println(p.toUpperCase());
+       
+       String g = "Computer Science is for nerds";
+       System.out.println(g.toLowerCase());
+       
+       //String c;
+       //String m = "The Gettysburg Address";
+       //c = m.substring(4);
+       
+       //System.out.println(c = m.substring(4));
+       
+       String b = "Four score and seven years ago";
+       String c = b.substring(7,12); 
+       
+       System.out.println(c = b.substring(7,12));
+       
+       int count;
+       //String s = "Surface tenseion";
+       System.out.println(count = s.length());
+	}
+	
+
+}
