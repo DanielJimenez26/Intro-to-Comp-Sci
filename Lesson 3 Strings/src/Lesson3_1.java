e80dddbe5be2bc27b39c51db8db2a3d8d252c148
djimenez <djimenez@HS-D210-16.MOHS.local>
2019-03-13 13:18:21 -0400

    3/13/19

--- Lesson 3 Strings/src/Lesson3_1.java
+++ Lesson 3 Strings/src/Lesson3_1.java
@@ -3,47 +3,47 @@ public class Lesson3_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       //String s = "The rabbits is ";
-       //int argh = 129;
-       //String punc = ".";
-       //String report = s + argh + punc;
-       //System.out.println(report);
+       String s = "The rabbits is ";
+       int argh = 129;
+       String punc = ".";
+       String report = s + argh + punc;
+       System.out.println(report);
        
-       //String p = "GroovyDude";
-       //System.out.println(p.toUpperCase());
+       String p = "GroovyDude";
+       System.out.println(p.toUpperCase());
        
-       //String g = "Computer Science is for nerds";
-       //System.out.println(g.toLowerCase());
+       String g = "Computer Science is for nerds";
+       System.out.println(g.toLowerCase());
      
-       //String c;
-       //String m = "The Gettysburg Address";
-      // c = m.substring(4);
+       String c;
+       String m = "The Gettysburg Address";
+       c = m.substring(4);
        
-       //System.out.println(c = m.substring(4));
-       
+       System.out.println(c = m.substring(4));
+     
        //String b = "Four score and seven years ago";
        //String c = b.substring(7,12); 
      
        //System.out.println(c = b.substring(7,12));
 		
-	   int count; 
-       String s = "Surface tension";
-       System.out.println(count = s.length());
+	   //int count; 
+       //String s = "Surface tension";
+       //System.out.println(count = s.length());
 		
 		//String m = "Look here!";
 		//int count;
 		//System.out.println(count = m.length());
 		
-		//System.out.println("All \"good\" men should come to the aid of their country");
+		System.out.println("All \"good\" men should come to the aid of their country");
 		
-		//System.out.println("Hello \nHello again");
+		System.out.println("Hello \nHello again");
 		
-		//System.out.println("A backslash looks like this \\, ...right?");
+		System.out.println("A backslash looks like this \\, ...right?");
 		
-		//String pq = "Eddie Haskel";
-		//int hm = pq.length();
-		//String ed = pq.substring(hm - 4);
-		//System.out.println(hm);
+		String pq = "Eddie Haskel";
+		int hm = pq.length();
+		String ed = pq.substring(hm - 4);
+		System.out.println(hm);
 		
 	}
 	
