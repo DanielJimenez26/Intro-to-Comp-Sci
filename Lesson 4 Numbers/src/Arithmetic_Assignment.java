f86498b651dd544cd2bb8641767eb67e80e25705
djimenez <djimenez@HS-D210-16.MOHS.local>
2019-03-14 12:10:28 -0400

    3/19/19

---
+++ Lesson 4 Numbers/src/Arithmetic_Assignment.java
@@ -0,0 +1,34 @@
+
+public class Arithmetic_Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int num1 = 79;
+       int num2 = 3;
+       int num3 = 4;
+       int num4 = 82;
+       int num5 = 68;
+       int num6 = 7;
+       int num7 = 19;
+       int num8 = 179;
+       int num9 = 21;
+       int num10 = 10;
+       int num11 = 181;
+       int num12 = 10389;
+       int num13 = 56;
+       int num14 = 11;
+       int num15 = 2246;
+       int answ1 = 145;
+       int answ2 = 211;
+       int answ3 = 6401870;
+       
+       System.out.println(num1 + "+" + num2 + "*" + "(" + num3 + "+" + num4 + "-" + num5 + ")" + "-" + num6 + "+" + num7);
+       System.out.println("(" + num8 + "+" + num9 + "+" + num10 + ")" + "/" + num6 + "+" + num11);
+       System.out.println(num12 + "*" + num13 + "*" + num14 + "+" + num15);
+       System.out.println("\n");
+       System.out.println(num1 + "+" + num2 + "*" + "(" + num3 + "+" + num4 + "-" + num5 + ")" + "-" + num6 + "+" + num7 + "=" + answ1);
+	   System.out.println("(" + num8 + "+" + num9 + "+" + num10 + ")" + "/" + num6 + "+" + num11 + "=" + answ2);
+	   System.out.println(num12 + "*" + num13 + "*" + num14 + "+" + num15 + "=" + answ3);
+	}
+
+}
