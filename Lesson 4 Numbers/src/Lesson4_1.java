2e8a928c412a69d600574a4a653cd1e7273c15d6
djimenez <djimenez@HS-D210-16.MOHS.local>
2019-03-14 11:31:34 -0400

    3/14/19

---
+++ Lesson 4 Numbers/src/Lesson4_1.java
@@ -0,0 +1,42 @@
+
+public class Lesson4_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int h = 103;
+     //int p = 5;
+     //System.out.println(++h + p);
+     System.out.println(h);
+     
+     double def;
+     double f = 1992.37;
+     def = f;
+     System.out.println(def);
+      
+     int p = 40;
+     int q = 4;
+     System.out.println(2+8*q/2-p);
+     
+     int sd = 12;
+     int x = 4;
+     System.out.println(sd%(++x));
+     System.out.println(x);
+     
+     int g;
+     //3 = g;
+     //System.out.println(++g*79);
+     
+     int m = 36;
+     //int j = 5;
+    // m = m/j;
+     System.out.println(m);
+     
+     System.out.println(3/4+5*2/33-3+8*3);
+     
+     int j = 2;
+     System.out.println(7%3+j+++(j-2));
+     
+     
+	}
+
+}
---
+++ Lesson 4 Numbers/src/Numbers.java
@@ -0,0 +1,23 @@
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//int x=7;
+//int X=5;
+//System.out.println(x);
+int i = 5;
+System.out.println(i++);
+System.out.println(i);
+System.out.println(i++);
+System.out.println(i);
+System.out.println(++i);
+System.out.println(i);
+System.out.println(--i);
+System.out.println(i+++2);
+System.out.println(i);
+System.out.println(i+++2*i++);
+
+	}
+
+}
