512779ccce015ac69190daf5c69d96dd66241257
djimenez <djimenez@HS-LIBCL-16.MOHS.local>
2019-04-26 08:19:50 -0400

    test review lesson 1-4

---
+++ Lesson 1-4 TEST Review/src/Test_Review.java
@@ -0,0 +1,57 @@
+
+public abstract class Test_Review {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      //int x = 3;
+      //int y = 5;
+      //double a = 2.5;
+      //System.out.println(x*a);
+      //System.out.println(y/a);
+      //System.out.println(x/y);
+      //System.out.println(x-y*a);
+      //System.out.println((x+x+y)*a);
+      //System.out.println(y%x*x+y);
+      
+      System.out.println("\tHello there\nMy Friend");
+      System.out.println("\"C:\\My Documents\\Pictures\"");
+      System.out.println("/\nThis\n\"is\"\nmany\n\nmany\nlines\nwith\none\\blank");
+      
+      System.out.println("My name is \"Mrs. Nesbit\"!");
+      System.out.println("How\nDid\ni\ndo it?");
+      System.out.println("\tC:\\\nC:\\spot\nC:\\spot\\run\nRun\\spot\\run");
+      
+      String name = "Johnny";
+      String mName = "Know";
+      String lName = "Knocks";
+      String longWord = "And Then They Came For Me";
+      int age = 20;
+      int age2 = 22;
+      
+      System.out.println(name + mName + lName);
+      System.out.println((name + lName).length());
+      System.out.println("whoDunit".length());
+      System.out.println(lName + "is" + age + age2 + "years old!");
+      System.out.println(name.toUpperCase() + mName.toLowerCase() + lName.toUpperCase());
+      System.out.println((name + mName + lName).toUpperCase() + " Is ThE NaME".toLowerCase());
+      System.out.println(longWord.substring(10));
+      System.out.println(longWord.substring(4,15));
+      System.out.println(longWord.substring(19,22) + longWord.substring(4,13));
+      
+      int a = 5;
+      int b = 7;
+      double x = 10.5;
+      double y = a;
+      
+     System.out.println(x);
+     System.out.println(a);
+     System.out.println(a++);
+     System.out.println(++a);
+     
+     String word = "bikinibottom";
+     System.out.println(word.substring(word.length()/2) + word.substring(0,word.length()/2));
+   
+	}
+	
+
+}
