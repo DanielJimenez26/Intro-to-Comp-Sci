0d010e221271a354b4d02e3cf60e6db6116109ed
djimenez <djimenez@HS-D210-16.MOHS.local>
2019-04-26 14:14:55 -0400

    4/26/19

---
+++ Lesson 6 - Math Class/src/Lesson_6_Math_Class.java
@@ -0,0 +1,12 @@
+
+public class Lesson_6_Math_Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double k = Math.abs(-127.5);
+		System.out.println(k);
+		
+		
+	}
+
+}
