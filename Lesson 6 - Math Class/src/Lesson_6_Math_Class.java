485c07033a784c9289e0103feeab822c36de1f86
djimenez <djimenez@HS-D210-16.MOHS.local>
2019-04-29 13:21:31 -0400

    4/29/19

--- Lesson 6 - Math Class/src/Lesson_6_Math_Class.java
+++ Lesson 6 - Math Class/src/Lesson_6_Math_Class.java
@@ -6,6 +6,24 @@ public class Lesson_6_Math_Class {
 		double k = Math.abs(-127.5);
 		System.out.println(k);
 		
+		int A = 2;
+		double B = 1.5;
+		System.out.println(Math.pow(A, B));
+		
+		double x = 16;
+		double y = Math.sqrt(x);
+		System.out.println(y);
+		
+		System.out.println(Math.ceil(-157.2));
+		System.out.println(Math.floor(-157.2));
+		System.out.println(Math.ceil(157.2));
+		System.out.println(Math.floor(157.2));
+		System.out.println(Math.round(-157.2));
+		System.out.println(Math.ceil(-157.7));
+		System.out.println(Math.ceil(157));
+		System.out.println(Math.ceil(157.7));
+		System.out.println(Math.log(18));
+		
 		
 	}
 
