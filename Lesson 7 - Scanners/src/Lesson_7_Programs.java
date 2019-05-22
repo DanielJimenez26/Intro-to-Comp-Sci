00464562e22e932a35df24c3e1fa6160e55d9464
djimenez <djimenez@HS-D210-16.MOHS.local>
2019-05-22 13:11:34 -0400

    5/22/19

--- Lesson 7 - Scanners/src/Lesson_7_Programs.java
+++ Lesson 7 - Scanners/src/Lesson_7_Programs.java
@@ -3,29 +3,30 @@ public class Lesson_7_Programs {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		Scanner name1 = new Scanner(System.in);
-//		int number;
-//		System.out.println("Please enter your name.");
-//		String name = name1.nextLine();
-//		System.out.println("Your name has" + " " + name.length() + " " + "characters in it, how many would you like me to print?");
-//		number = name1.nextInt();
-//		System.out.println(name.substring(0,number));
+/*		Scanner name1 = new Scanner(System.in);
+		int number;
+		System.out.println("Please enter your name.");
+		String name = name1.nextLine();
+		System.out.println("Your name has" + " " + name.length() + " " + "characters in it, how many would you like me to print?");
+		number = name1.nextInt();
+		System.out.println(name.substring(0,number));
 		
-//		Scanner name2 = new Scanner(System.in);
-//		int number;
-//		System.out.println("Please enter your whole name.");
-//		String name = name2.nextLine();
-//		System.out.println("Your name has" + " " + name.length() + " " + "characters in it, how many would like me to print?");
-//		number = name2.nextInt();
-//		System.out.println(name.substring(0,number));
+		Scanner name2 = new Scanner(System.in);
+		int number;
+		System.out.println("Please enter your whole name.");
+		String name = name2.nextLine();
+		System.out.println("Your name has" + " " + name.length() + " " + "characters in it, how many would like me to print?");
+		number = name2.nextInt();
+		System.out.println(name.substring(0,number));
 		
-//		Scanner math1 = new Scanner(System.in);
-//		System.out.println("Please enter your A value");
-//		int math = math1.nextInt();
-//		System.out.println("Please enter your B value");
-//		int math2 = math1.nextInt();
-//		double answer = Math.sqrt(Math.pow(math, 2) + Math.pow(math2,2));
-//		System.out.println("Square root of 3.0 sqaured + 4.0 squared =" + " " + answer);
+		Scanner math1 = new Scanner(System.in);
+		System.out.println("Please enter your A value");
+		int math = math1.nextInt();
+		System.out.println("Please enter your B value");
+		int math2 = math1.nextInt();
+		double answer = Math.sqrt(Math.pow(math, 2) + Math.pow(math2,2));
+		System.out.println("Square root of 3.0 sqaured + 4.0 squared =" + " " + answer);
+		
 		
 		Scanner quad = new Scanner(System.in);
 		System.out.println("Please enter the A value");
@@ -37,7 +38,20 @@ public class Lesson_7_Programs {
 		double math4 = (-math2 + Math.sqrt(Math.pow(math2, 2) - (4*math*math3))) / (2*math);
 		double math5 = (-math2 - Math.sqrt(Math.pow(math2, 2) - (4*math*math3))) / (2*math);
 		System.out.println("Plus" + " " + "=" + " " + math5);
-		System.out.println("Minus" + " " + "=" + " " + math4);
+		System.out.println("Minus" + "" + " " + "=" + " " + math4);
+		*/
+		
+		Scanner calc = new Scanner(System.in);
+		System.out.println("Please enter the A value");
+		double maths = calc.nextDouble();
+		System.out.println("Please enter the B value");
+		double mathz = calc.nextDouble();
+		System.out.println("Please enter the C value");
+		double mathez = calc.nextDouble();
+		double math1 = (-mathz + Math.sqrt(Math.pow(mathz, 2) - (4*maths*mathez))) / (2*maths);
+		double math2 = (-mathz - Math.sqrt(Math.pow(mathz, 2) - (4*maths*mathez))) / (2*maths);
+		System.out.println("Plus" + " " + " " + "=" + " " + math1);
+		System.out.println("Minus" + " " + " " + "=" + " " + math2);
 		
 	}
 
