9b21acb0cd35d8f2091a088891035b657c32bdf2
djimenez <djimenez@HS-D210-16.MOHS.local>
2019-06-07 13:21:09 -0400

    6/7/19

--- Lesson 9 - If Boolean/src/Lesson_9_If_Statements.java
+++ Lesson 9 - If Boolean/src/Lesson_9_If_Statements.java
@@ -3,37 +3,39 @@ public class Lesson_9_If_Statements {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		boolean true_false;
-//		int i = 10, j = 3;
-//		true_false = (j>i);
-//		true_false = (i>j);
-//		true_false = (i==j);
-//		true_false = ((j<=i)||(j>=i));
-//	    true_false = ((i>j)&&(j==0));
-//	    true_false = ((j<50)||(j!=33));
-//	    true_false = (!(j>=0)||(i<=50));
-//	    true_false = (!(!(!true)));
-//	    true_false = (5<=5);
-//	    true_false = (j!=i);
-//		System.out.println(true_false);
-//		
-//		boolean b;
-//		int m = 36;
-//		b = (m<=44) ;
-//		System.out.println(b);
-//
-//		String s1 = "school BUS";
-//		if(s1.equals("school bus"))
-//			System.out.println("Equal");
-//		else
-//			System.out.println("Not Equal");
-//		
-//		int j=19, m=200;
-//		if (j==18)
-//			m++;
-//			j++;
-//		System.out.println(m);
-//		System.out.println(j);
+		boolean true_false;
+		int i = 10, j = 3;
+		true_false = (j>i);
+		true_false = (i>j);
+		true_false = (i==j);
+		true_false = ((j<=i)||(j>=i));
+	    true_false = ((i>j)&&(j==0));
+	    true_false = ((j<50)||(j!=33));
+	    true_false = (!(j>=0)||(i<=50));
+	    true_false = (!(!(!true)));
+	    true_false = (5<=5);
+	    true_false = (j!=i);
+		System.out.println(true_false);
+		
+		boolean b;
+		int m = 36;
+		b = (m<=44) ;
+		System.out.println(b);
+
+		String s1 = "school BUS";
+		if(s1.equals("school bus"))
+			System.out.println("Equal");
+		else
+			System.out.println("Not Equal");
+		
+		int d=19, x=200;
+		if (j==18)
+			x++;
+			d++;
+		System.out.println(x);
+		System.out.println(d);
+		
+		
 		
 		
 		
