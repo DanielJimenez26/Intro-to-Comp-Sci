773434be2ac360f28fb8186390e1270024a72a85
djimenez <djimenez@HS-D210-16.MOHS.local>
2019-06-13 13:21:30 -0400

    6/13/19

--- Final Review/src/Final_Review.java
+++ Final Review/src/Final_Review.java
@@ -12,10 +12,27 @@ public class Final_Review {
 		System.out.println("Enter another word.");
 		String word2 = InputScan.nextLine();
 		String both = word1 + word2;
+		System.out.println(both);
 		if (word1.length()==word2.length()) {
-			
+			System.out.println("Good Job!");
+		}else{
+			System.out.println("Almost!");
 			
 		}
+		Scanner Fax = new Scanner(System.in);
+		System.out.println("Enter a number.");
+		int g = Fax.nextInt();
+		System.out.println("Enter a word.");
+		String s1 = Fax.nextLine();
+		System.out.println("Enter another word.");
+		String s2 = Fax.nextLine();
+		if (s1.equalsIgnoreCase(s2)) {
+			System.out.println("The words are the same-ish.");
+		}else if (s1.length()+s2.length()==g) {
+			System.out.println("da same.");
+		}
+		
 	}
 
+	
 }
